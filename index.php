763ef24a2e14a6a67357565f1ff96befeac532f9
HanSangYoon <microscope83@gmail.com>
2019-08-11 10:46:32 +0900

    php practice initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meat charset="UTF-8">
+		<title>PHP practice</title?
+	</head>
+	<body>
+		<p>php 테스트를 합니다.</p>
+		<?php
+			echo "hello world";
+			echo "hello world2";
+		
+		?>
+	</body>
+
+</html>
